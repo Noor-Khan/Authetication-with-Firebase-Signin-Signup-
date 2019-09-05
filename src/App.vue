87f70d0aaf75f642a52d725819a3cee6d4ba6cb5
iksdatoo <noork9072@gmail.com>
2019-09-05 12:41:29 +0500

    second commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <router-link v-if="authenticated" to="/" v-on:click.native="logout()" replace>Logout</router-link>
+    <router-view @authenticated="setAuthenticated" />
+  </div>
+</template>
+<script>
+export default {
+   data() {
+      return {
+          authenticated: false,
+          mockAccount: {
+              username: "noor",
+              password: "passpass"
+          }
+      }
+    },
+    mounted() {
+        if(!this.authenticated) {
+            this.$router.replace({ name: "login" });
+        }
+    },
+    methods: {
+        setAuthenticated(status) {
+            this.authenticated = status;
+        },
+        logout() {
+            this.authenticated = false;
+        }
+    }
+}
+</script>
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h2>Dashboard</h2>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
